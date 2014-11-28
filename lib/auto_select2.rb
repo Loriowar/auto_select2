dc2a4eaac3fe7e1c71a777d8c13c4f5091be71ba
Ivan Zabrovskiy <loriowar@gmail.com>
2014-11-28 17:03:36 +0300

    Work with autoload
     - engine move to the separate file
     - remove module for custome SearchAdapter

--- lib/auto_select2.rb
+++ lib/auto_select2.rb
@@ -1,16 +1,10 @@
 require 'auto_select2/version'
 require 'auto_select2/helpers'
+require 'auto_select2/engine'
 require 'select2_search_adapter/base'
 
 module AutoSelect2
-  class Engine < ::Rails::Engine
-    # Get rails to add app, lib, vendor to load path
+  extend ActiveSupport::Autoload
 
-    initializer :javascripts do |app|
-      app.config.assets.precompile +=
-          %w(auto_select2/ajax_select2.js
-             auto_select2/multi_ajax_select2_value_parser.js
-             auto_select2/static_select2.js)
-    end
-  end
+  autoload :Select2SearchAdapter
 end
